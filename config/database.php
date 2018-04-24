f69fc3ea21d8e8bcfdcc97ac591641255a607373
Xodious <jr0850050@otc.edu>
2018-04-23 22:24:33 -0500

    adding some forgotten database.php code

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 
-/* if ($url = env('CLEARDB_DATABASE_URL')) {
+if ($url = env('CLEARDB_DATABASE_URL')) {
     $parts = parse_url($url);
     putenv('DB_HOST=' . $parts['host']);
     putenv('DB_USERNAME=' . $parts['user']);
     putenv('DB_PASSWORD=' . $parts['pass']);
     putenv('DB_DATABASE=' . substr($parts['path'], 1));
-} */
+}
 
 return [
 
