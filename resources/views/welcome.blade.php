3628e3e01c323098bc8e66d30c50ec7862789210
Xodious <jr0850050@otc.edu>
2018-04-24 19:51:18 -0500

    pls

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,14 +17,14 @@ Copyright 2018
     <title>One Page Wonder - Start Bootstrap Template</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="/css/one-page-wonder.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="/css/one-page-wonder.min.css">
 
   </head>
 
